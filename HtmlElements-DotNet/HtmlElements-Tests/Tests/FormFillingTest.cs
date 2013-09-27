5779518551594c04346490b1c9f25d18d2d075ad
Vadzim Hushchanskou <uv@mail.by>
2013-09-27 16:41:47 +0300

    A Unit tests project with the first test was added

---
+++ HtmlElements-DotNet/HtmlElements-Tests/Tests/FormFillingTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yandex.HtmlElements.Elements;
+using OpenQA.Selenium;
+using Moq;
+using HtmlElements.Tests.TestElements;
+using System.Collections.Generic;
+
+namespace HtmlElements.Tests
+{
+    [TestClass]
+    public class FormFillingTest
+    {
+        private const String InputTextToSend = "text";
+        private const bool CheckboxValueToSet = true;
+
+        private Mock<IWebElement> mockedElement;
+
+        private MockedForm form;
+
+        [TestInitialize]
+        public void fillForm()
+        {
+            mockedElement = new Mock<IWebElement>();
+            form = new MockedForm(mockedElement);
+
+            // Prepare data to fill form with
+            IDictionary<string, object> data = new Dictionary<string, object>();
+            data[MockedForm.TextInputName] = InputTextToSend;
+            data[MockedForm.CheckboxName] = CheckboxValueToSet;
+            data[MockedForm.RadioName] = MockedForm.RadioButtonValue;
+            data[MockedForm.SelectName] = MockedForm.SelectOptionValue;
+            data[MockedForm.TextAreaName] = InputTextToSend;
+
+            // Fill form
+            form.Fill(data);
+        }
+
+        [TestMethod]
+        public void FormFieldsShouldBeFilledCorrectly()
+        {
+            form.TextInput.Verify(m => m.SendKeys(InputTextToSend));
+            form.CheckBox.Verify(m => m.Click());
+            form.RadioButton.Verify(m => m.Click());
+            form.SelectOption.Verify(m => m.Click());
+            form.TextArea.Verify(m => m.SendKeys(InputTextToSend));
+        }
+    }
+}
---
+++ HtmlElements-DotNet/HtmlElements-Tests/Tests/TestElements/MockedForm.cs
@@ -0,0 +1,118 @@
+﻿using Moq;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yandex.HtmlElements.Elements;
+
+namespace HtmlElements.Tests.TestElements
+{
+    public class MockedForm : Form
+    {
+        public const string TextInputName = "textinput";
+        public const string CheckboxName = "checkbox";
+        public const string SelectName = "select";
+        public const string RadioName = "radio";
+        public const string TextAreaName = "textarea";
+
+        public const String RadioButtonValue = "value";
+        public const String SelectOptionValue = "value";
+
+        private readonly Mock<IWebElement> mockedElement;
+
+        private readonly Mock<IWebElement> textInput;
+        private readonly Mock<IWebElement> checkBox;
+        private readonly Mock<IWebElement> selectOption;
+        private readonly Mock<IWebElement> radioButton;
+        private readonly Mock<IWebElement> textArea;
+
+        public MockedForm(Mock<IWebElement> mockedElement)
+            : base(mockedElement.Object)
+        {
+            this.mockedElement = mockedElement;
+            textInput = MockTextInput();
+            checkBox = MockCheckBox();
+            selectOption = MockSelectOption();
+            radioButton = MockRadioButton();
+            textArea = MockTextArea();
+        }
+
+        private Mock<IWebElement> MockInputWithNameAndType(string name, string type)
+        {
+            Mock<IWebElement> element = new Mock<IWebElement>();
+            element.Setup(m => m.TagName).Returns("input");
+            element.Setup(m => m.GetAttribute("name")).Returns(name);
+            element.Setup(m => m.GetAttribute("type")).Returns(type);
+            return element;
+        }
+
+        private Mock<IWebElement> MockTextInput()
+        {
+            Mock<IWebElement> textInput = MockInputWithNameAndType(TextInputName, "text");
+            ReadOnlyCollection<IWebElement> textInputList = new List<IWebElement> { textInput.Object }.AsReadOnly();
+            mockedElement.Setup(m => m.FindElements(By.Name(TextInputName))).Returns(textInputList);
+            return textInput;
+        }
+
+        private Mock<IWebElement> MockCheckBox()
+        {
+            Mock<IWebElement> checkBox = MockInputWithNameAndType(CheckboxName, "checkbox");
+            ReadOnlyCollection<IWebElement> checkBoxList = new List<IWebElement> { checkBox.Object }.AsReadOnly();
+            mockedElement.Setup(m => m.FindElements(By.Name(CheckboxName))).Returns(checkBoxList);
+            return checkBox;
+        }
+
+        private Mock<IWebElement> MockRadioButton()
+        {
+            Mock<IWebElement> radioButton = MockInputWithNameAndType(RadioName, "radio");
+            ReadOnlyCollection<IWebElement> radioList = new List<IWebElement> { radioButton.Object }.AsReadOnly();
+            mockedElement.Setup(m => m.FindElements(By.Name(RadioName))).Returns(radioList);
+            radioButton.Setup(m => m.FindElements(By.XPath(string.Format("self::* | following::input[@type = 'radio' " +
+                "and @name = '{0}'] | preceding::input[@type = 'radio' and @name = '{0}']",
+                RadioName)))).Returns(radioList);
+            radioButton.Setup(m => m.GetAttribute("value")).Returns(RadioButtonValue);
+            return radioButton;
+        }
+
+        private Mock<IWebElement> MockSelectOption()
+        {
+            Mock<IWebElement> select = new Mock<IWebElement>();
+            Mock<IWebElement> selectOption = new Mock<IWebElement>();
+            select.Setup(m => m.TagName).Returns("select");
+            select.Setup(m => m.GetAttribute("name")).Returns(SelectName);
+            ReadOnlyCollection<IWebElement> selectList = new List<IWebElement> { select.Object }.AsReadOnly();
+            mockedElement.Setup(m => m.FindElements(By.Name(SelectName))).Returns(selectList);
+            ReadOnlyCollection<IWebElement> selectOptionList = new List<IWebElement> { selectOption.Object }.AsReadOnly();
+            select.Setup(m => m.FindElements(By.XPath(string.Format(".//option[@value = \"{0}\"]", SelectOptionValue)))).Returns(selectOptionList);
+            return selectOption;
+        }
+
+        private Mock<IWebElement> MockTextArea()
+        {
+            Mock<IWebElement> textArea = new Mock<IWebElement>();
+            ReadOnlyCollection<IWebElement> textAreaList = new List<IWebElement> { textArea.Object }.AsReadOnly();
+            mockedElement.Setup(m => m.FindElements(By.Name(TextAreaName))).Returns(textAreaList);
+            textArea.Setup(m => m.TagName).Returns("textarea");
+            textArea.Setup(m => m.GetAttribute("name")).Returns(TextAreaName);
+            return textArea;
+        }
+
+        public Mock<IWebElement> TextInput
+        { get { return textInput; } }
+
+        public Mock<IWebElement> CheckBox
+        { get { return checkBox; } }
+
+        public Mock<IWebElement> SelectOption
+        { get { return selectOption; } }
+
+        public Mock<IWebElement> RadioButton
+        { get { return radioButton; } }
+
+        public Mock<IWebElement> TextArea
+        { get { return textArea; } }
+    }
+}
